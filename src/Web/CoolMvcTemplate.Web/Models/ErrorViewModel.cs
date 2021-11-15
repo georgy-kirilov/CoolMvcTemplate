04ea0cadc206b7291ba43439bc3934a515790f20
georgy-kirilov <georgynk17@gmail.com>
2021-11-15 22:17:11 +0200

    Create template folder layout

---
+++ src/Web/CoolMvcTemplate.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoolMvcTemplate.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
