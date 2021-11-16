8e324fb33a60ddd984d3b5b73acd906839601fb4
georgy-kirilov <georgynk17@gmail.com>
2021-11-16 10:07:39 +0200

    Rename /Tests projects

---
+++ src/Web/CoolMvcTemplate.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolMvcTemplate.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
